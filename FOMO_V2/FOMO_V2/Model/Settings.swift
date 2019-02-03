1b0a4c480c30bfe2ebedb0a43cf0aa69615f1be0
Alex <administrator@MacBook-Pro-36.local>
2019-02-03 15:16:04 -0800

    added menu for video 8

---
+++ FOMO_V2/FOMO_V2/Model/Settings.swift
@@ -0,0 +1,9 @@
+//
+//  Settings.swift
+//  FOMO_V2
+//
+//  Created by Alex  on 2/3/19.
+//  Copyright © 2019 Samuel Kyu-Seo Lee. All rights reserved.
+//
+
+import Foundation
---
+++ FOMO_V2/FOMO_V2/View/SettingCell.swift
@@ -0,0 +1,9 @@
+//
+//  SettingCell.swift
+//  FOMO_V2
+//
+//  Created by Alex  on 2/3/19.
+//  Copyright © 2019 Samuel Kyu-Seo Lee. All rights reserved.
+//
+
+import Foundation
